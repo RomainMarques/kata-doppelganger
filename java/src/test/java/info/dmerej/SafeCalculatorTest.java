8abb0072e40866e8d6c08871b1d5f7b894e45f93
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2023-11-22 15:13:30 +0100

    Update SafeCalculatorTest.java

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -1,10 +1,18 @@
 package info.dmerej;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
     // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+    Authorizer authorizer = () -> true;
+    int left = 1;
+    int right = 2;
+
+    SafeCalculator calculator = new SafeCalculator(authorizer);
+    int result = calculator.add(left, right);
+    Assertions.assertEquals(left + right, result);
   }
 }
